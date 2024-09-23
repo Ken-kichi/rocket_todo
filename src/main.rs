1445f94a4aa83997103b57f40c97fdcd28031ddb
Ken-kichi <PoTi15673cmgpjwp@yahoo.co.jp>
2024-09-23 13:27:28 +0900

    Webの初期設定

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello world")
+use rocket::get;
+use rocket::routes;
+
+#[get("/")]
+pub fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    let _rocket = rocket::build().mount("/", routes![index]).launch().await?;
+
+    Ok(())
 }
